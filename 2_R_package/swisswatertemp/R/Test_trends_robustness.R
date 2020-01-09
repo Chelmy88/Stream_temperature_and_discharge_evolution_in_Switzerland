3dcb56a99367a717f6bc49d6d64cbce3fe4d925c
Chelmy88 <adrien.michel@protonmail.com>
2020-01-09 10:51:11 +0100

    Added proper header section to souce code files

--- 2_R_package/swisswatertemp/R/Test_trends_robustness.R
+++ 2_R_package/swisswatertemp/R/Test_trends_robustness.R
@@ -1,10 +1,24 @@
-# This script is related to the paper:
-# "Stream temperature evolution in Switzerland over the last 50 years, Adrien
-# Michel, Tristan Brauchli, Michael Lehning, Bettina Schaefli, and Hendrik Huwald,
-# HESS, 2019"
-
+###################################################################################
+# This content is related to the paper:
+#
+# Stream temperature and discharge evolution in Switzerland over the last 50 years:
+# annual and seasonal behaviour
+#
+# Adrien Michel, Tristan Brauchli, Michael Lehning, Bettina Schaefli,
+# and Hendrik Huwald
+#
+# Hydrol. Earth Syst. Sci., 2020
+#
+#
+# Any use of the material (code or data) presented here should clearly reference
+# to this paper and to the providers of the data mentioned in the documentation.
+#
 # This material is distributed under the GPLv3 license
 # (https://www.gnu.org/licenses/gpl-3.0.html)
+#
+# Author: Adrien Michel, adrien.michel@epfl.ch, 01.2020
+###################################################################################
+
 
 #' Produce tend robustness plots for T, Q, TA and P.
 #'
